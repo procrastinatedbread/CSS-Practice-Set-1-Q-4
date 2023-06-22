bd49846a5a64c3d0176af76fe51edb908ad1ed2b
Nikhil Chhabra <91406971+procrastinatedbread@users.noreply.github.com>
2023-06-22 21:37:34 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<form>
+<input type="email" placeholder="Enter your email address"/>
+<button type="submit">Submit</button>
+<form>
+`;
